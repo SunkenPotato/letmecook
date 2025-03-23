71244927f64205c57419c9351aa9c3a42e8bf55e
SunkenPotato <3.14radius02@gmail.com>
2025-03-23 16:22:59 +0100

    feat(users): prepare authentication header

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 pub mod user;
 mod utils;
 
+use std::sync::LazyLock;
+
+use rand::Rng;
 use rocket_db_pools::Database;
 use user::UserModule;
 use utils::RocketExt;
+
+// TODO: use include! for a constant key
+static KEY: LazyLock<[u8; 512]> = LazyLock::new(|| {
+    let mut rand = rand::rng();
+    let mut seq = [0u8; 512];
+    rand.fill(&mut seq);
+
+    seq
+});
 
 #[derive(Database)]
 #[database("lmc")]
--- src/user/auth.rs
+++ src/user/auth.rs
@@ -1,1 +1,57 @@
+use chrono::Utc;
+use jsonwebtoken::{DecodingKey, EncodingKey, Header, Validation, decode, encode, errors::Error};
+use rocket::{
+    Request,
+    http::Status,
+    request::{FromRequest, Outcome},
+};
+use serde::{Deserialize, Serialize};
 
+struct Authentication<'r>(&'r str);
+
+#[derive(Debug)]
+enum AuthenticationError {
+    Invalid,
+    Missing,
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for Authentication<'r> {
+    type Error = AuthenticationError;
+
+    async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        match req.headers().get_one("Authentication") {
+            None => Outcome::Error((Status::BadRequest, AuthenticationError::Missing)),
+            Some(key) if validate_auth_key(key) => Outcome::Success(Authentication(key)),
+            Some(_) => {
+                rocket::outcome::Outcome::Error((Status::BadRequest, AuthenticationError::Invalid))
+            }
+        }
+    }
+}
+
+fn validate_auth_key(key: &str) -> bool {
+    let validation = Validation::new(jsonwebtoken::Algorithm::HS512);
+
+    decode::<Claims>(key, &DecodingKey::from_secret(&*crate::KEY), &validation).is_ok()
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+    sub: u32,
+    exp: i64,
+}
+
+impl Claims {
+    fn new(sub: u32) -> Result<String, Error> {
+        let claims = Self {
+            sub,
+            exp: Utc::now().timestamp() + 10800,
+        };
+
+        let header = Header::new(jsonwebtoken::Algorithm::HS512);
+        let token = encode(&header, &claims, &EncodingKey::from_secret(&*crate::KEY));
+
+        dbg!(token)
+    }
+}
