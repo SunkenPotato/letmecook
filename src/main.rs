dccbf86eef7361e1bf77a08a5f89c0597ac25935
SunkenPotato <3.14radius02@gmail.com>
2025-03-26 14:42:43 +0100

    ops: add recipe datastructure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod recipe;
 pub mod user;
 mod utils;
 
---
+++ src/recipe/mod.rs
@@ -0,0 +1,70 @@
+use rocket::routes;
+use serde::{Deserialize, Serialize};
+
+use crate::utils::Module;
+
+pub struct RecipeModule;
+
+impl Module for RecipeModule {
+    const BASE_PATH: &str = "/recipe";
+
+    fn routes() -> Vec<rocket::Route> {
+        routes![]
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct Recipe {
+    origin: Option<String>,
+    #[serde(rename = "preparationTime")]
+    preparation_time: u64, // in s
+    #[serde(rename = "cookingTime")]
+    cooking_time: u64, // in s
+    ingredients: Vec<Ingredient>,
+    steps: Vec<RecipeStep>,
+}
+
+#[derive(Serialize, Deserialize)]
+enum Quantity {
+    #[serde(rename = "l")]
+    Liters(f32),
+    #[serde(rename = "g")]
+    Grams(u32),
+    #[serde(rename = "n")]
+    Count(u32),
+}
+
+#[derive(Serialize, Deserialize)]
+struct Ingredient {
+    name: String,
+    quantity: Quantity,
+}
+
+#[derive(Serialize, Deserialize)]
+struct RecipeStep(String); // this isn't worth parsing any further
+
+#[cfg(test)]
+mod tests {
+    use super::Recipe;
+
+    #[test]
+    fn parse_recipe() {
+        let input = r#"
+            {
+                "origin": "DE",
+                "preparationTime": 1200,
+                "cookingTime": 1200,
+                "ingredients": [
+                    {
+                        "name": "Tomato",
+                        "quantity": { "n": 5 }
+                    }
+                ],
+                "steps": ["...."]
+            }
+
+            "#;
+
+        serde_json::from_str::<Recipe>(&input).unwrap();
+    }
+}
